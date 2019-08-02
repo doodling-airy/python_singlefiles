d67768c310805427e1e7d3d7b164af61d3cc2a4f
doodling-airy <52639057+doodling-airy@users.noreply.github.com>
2019-08-02 17:32:52 +0900

    Update repeat_for_verilog.py

--- repeat_for_verilog.py
+++ repeat_for_verilog.py
@@ -6,7 +6,7 @@ class Verilog:
         self.verilog_filename = input("verilog file name : ")
         self.verilog_filestream = open(self.verilog_filename, "r")
         self.filedata = []
-        self.seeds = set(['x', 'h'])
+        self.seeds = set(['x', 'd', 'b', 'o'])
     def __del__(self):
         self.verilog_filestream.close()
 
@@ -16,7 +16,7 @@ class Verilog:
 
     def finder(self):
         self.sharp = self.line.find("#")
-        if(self.sharp != -1): 
+        if(self.sharp != -1):
             self.x = self.line[self.sharp+2]
             self.replaceting(self.line[self.sharp+1])
         else:
@@ -25,20 +25,22 @@ class Verilog:
     def replaceting(self, seed):
         if(seed in self.seeds):
             for i in range(int(re.sub("\\D", "", self.line[self.sharp+3:self.sharp+3 + int(self.x)]))):
-                print(format(i, "02" + seed ).upper())
                 self.lined = self.line.replace("#" + seed + self.line[self.sharp+2:self.sharp+2 + int(self.x) + 1], format(i, "02" + seed ).upper())
                 self.filedata.append(self.lined)
+        else:
+            self.filedata.append(self.line)
     
     def out(self):
-        path = r"C:\Users\iwaha\Documents\practice_6_14\\" + self.verilog_filename
+        path =  self.verilog_filename
         with open(path, mode='w') as f:
             f.write("".join(self.filedata))
                 
         cmd = 'code ' + self.verilog_filename 
         os.system(cmd)
 
-vlog = Verilog()
-vlog.mainstream()
-vlog.out()
-del vlog
+if __name__ == "__main__":
+    vlog = Verilog()
+    vlog.mainstream()
+    vlog.out()
+    del vlog
 
